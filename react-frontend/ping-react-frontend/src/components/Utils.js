d685fb7af83d71d6afb596bc02d8b42d61496995
emeric.du-gardin <emeric.du-gardin@epita.fr>
2022-07-04 01:49:17 +0200

    fix: Voice feature

--- react-frontend/ping-react-frontend/src/components/Utils.js
+++ react-frontend/ping-react-frontend/src/components/Utils.js
@@ -1,12 +1,8 @@
+import { useEffect } from 'react';
+
 export default function spell() {
-    if ('speechSynthesis' in window) {
-        // Speech Synthesis supported
-        const highlighted = window.getSelection().toString();
-        let msg = new SpeechSynthesisUtterance();
-        msg.text = highlighted;
-        window.speechSynthesis.speak(msg);
-       } else{
-         // Speech Synthesis Not Supported
-         alert("Sorry, your browser doesn't support text to speech!");
-       }
+  const highlighted = window.getSelection().toString();
+  const msg = new SpeechSynthesisUtterance();
+  msg.text = highlighted;
+  window.speechSynthesis.speak(msg)
 }
